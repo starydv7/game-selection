0f4f6d9b4d0effd0216160425575fc0d0317b08e
starydv7 <pawanstar72926@gmail.com>
2023-07-06 11:13:28 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Home2 from './components/Country';
+import Filter from './components/Filter';
 
 function App() {
   return (
     <div className="App">
-      <Home />\
-      <Home2/>
+      <Home />
+      <hr/>
+      <Home2 />
+      <hr/>
+      <Filter/>
     </div>
   );
 }
---
+++ src/components/Filter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const employees = [
+  { name: "John", salary: 250, months: ["January", "February", "March"] },
+  { name: "Jane", salary: 350, months: ["April", "May", "June"] },
+  { name: "Bob", salary: 400, months: ["July", "August", "September"] },
+  { name: "Alice", salary: 200, months: ["October", "November", "December"] },
+];
+
+const FilteredEmployees = () => {
+  const filteredEmployees = employees.filter(
+    (employee) => employee.salary > 300
+  );
+
+  return (
+    <div>
+      {filteredEmployees.map((employee) => (
+        <div key={employee.name}>
+          <p>Name: {employee.name}</p>
+          <p>Months: {employee.months.join(", ")}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FilteredEmployees;
